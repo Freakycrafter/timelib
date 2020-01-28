a0c2cd99bf0a7e35fb5432a366259746b337b683
Freakycrafter <marc.bosau@gmail.com>
2020-01-28 11:21:01 +0100

    "functionised" main method

--- main.c
+++ main.c
@@ -6,39 +6,91 @@
 *   Datum: 14.01.20
 *   Program zur Berechnung des aktuellen Tages im Jahr
 **/
-bool istSchaltjahr(int jahr)
+int is_leapyear(int year)
 {
-    if(jahr % 4 != 0)
+    if(year >= 1581 && year <= 2400)
     {
-        printf("Jahr ist kein Schaltjahr.\n");             //Nicht durch vier teilbar -> kein Schaltjahr
-        return false;
-    }
-    else
-    {
-        if(jahr % 100 != 0)
+
+        if(year % 4 != 0)
         {
-            printf("Jahr ist ein Schaltjahr.\n");          //Durch vier aber nicht durch hundert teilbar -> Schaltjahr
-            return true;
+            //printf("Jahr ist kein Schaltjahr.\n");             //Nicht durch vier teilbar -> kein Schaltjahr
+            return 0;
         }
         else
         {
-            if(jahr % 400 != 0 )
+            if(year % 100 != 0)
             {
-                printf("Jahr ist kein Schaltjahr.\n");     //Durch hundert teilbar, aber nicht durch vierhundert teilbar -> kein Schaltjahr
-                return false;
+                //printf("Jahr ist ein Schaltjahr.\n");          //Durch vier aber nicht durch hundert teilbar -> Schaltjahr
+                return 1;
             }
             else
             {
-                printf("Jahr ist ein Schaltjahr.\n");      //Durch vierhundert teilbar -> Schaltjahr
-                return true;
+                if(year % 400 != 0 )
+                {
+                    //printf("Jahr ist kein Schaltjahr.\n");     //Durch hundert teilbar, aber nicht durch vierhundert teilbar -> kein Schaltjahr
+                    return 0;
+                }
+                else
+                {
+                    //printf("Jahr ist ein Schaltjahr.\n");      //Durch vierhundert teilbar -> Schaltjahr
+                    return 1;
+                }
             }
         }
     }
+    return -1;
 }
 
-
-int main()
+int get_days_for_month(int month, int year)
 {
+    if(month >= 1 && month <= 12)
+    {
+    int days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    if(is_leapyear(year))
+    {
+        days[1]++;
+    }
+    return days[month-1];
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int exists_date(int day, int month, int year)
+{
+    int isLeapYear = is_leapyear(year);
+    int monthDayMax = get_days_for_month(month, year);
+    if(isLeapYear == 1 || isLeapYear == 0)
+    {
+        if(day > 0 && day <= monthDayMax)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int day_of_the_year(int day, int month, int year)
+{
+    int dayOfYear = 0;
+    if(exists_date(day, month, year))
+    {
+        for(int i = 0; i < month - 1; i++)
+        {
+            dayOfYear += get_days_for_month(month + 1, year);
+        }
+        dayOfYear += day;
+        return day;
+    }
+    return -1;
+}
+
+main()
+{
+    day_of_the_year(1, 1, 2000);
+    /**
     int tage[12] = {31,28,31,30,31,30,31,31,30,31,30,31};   //Anzahl Tage in den verschiedenen Monaten
     int tag;
     int monat;
@@ -69,5 +121,5 @@ int main()
     }
     aktuellerTag += tag;                                    //Addieren des aktuellen Tages im Monat
     printf("Der aktuelle Tag im Jahr ist der %i te Tag im Jahr. Dies ist die geupdatete Version.", aktuellerTag);
-    return 0;
+    return 0;**/
 }
